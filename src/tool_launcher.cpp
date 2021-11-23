bfdb865e5c96410e9f8d9bf355b973e9b2e9315a
Adrian Suciu <adrian.suciu@analog.com>
2021-11-23 12:18:52 +0200

    api: added focus instrument call
    Signed-off-by: Adrian Suciu <adrian.suciu@analog.com>

--- src/tool_launcher.cpp
+++ src/tool_launcher.cpp
@@ -1895,6 +1895,10 @@ void ToolLauncher::toolDetached(bool detached)
 	tool->setMinimumSize(910, 490);
 }
 
+enum tool ToolLauncher::getSelectedToolId() const {
+	return selectedToolId;
+}
+
 
 
 void ToolLauncher::closeEvent(QCloseEvent *event)
